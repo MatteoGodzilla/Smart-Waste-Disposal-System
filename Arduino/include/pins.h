315f0be0c863e60dbe18aa3e88ee4b74efcbb6b6
Lorenzo Carletti <lorenzo.carletti3@studio.unibo.it>
2024-11-21 15:10:49 +0100

    Added pins.h

---
+++ Arduino/include/pins.h
@@ -0,0 +1,14 @@
+#pragma once
+
+/*OUTPUT*/
+#define L1 12
+#define L2 13
+#define MOTOR 9
+
+/*INPUT*/
+#define OPEN 10
+#define CLOSE 11
+#define PIR 2
+#define TEMP A0
+#define SONAR_TRIG 8
+#define SONAR_ECHO 7
--- Arduino/src/main.cpp
+++ Arduino/src/main.cpp
@@ -1,5 +1,7 @@
 #include <Arduino.h>
 
+#include "pins.h"
+
 // put function declarations here:
 int myFunction(int, int);
 
