2b3f31f3f58e852ec683cb0fd60dd9084fda43a5
MatteoGodzilla <catena.matteo90@gmail.com>
2024-11-26 16:15:49 +0100

    .

--- Arduino/src/WasteTask.cpp
+++ Arduino/src/WasteTask.cpp
@@ -61,7 +61,16 @@ void WasteTask::changeState(){
     scTask->sendFillPercentage(fillPercentage);
     switch(fsm->state){
         case AVAILABLE:
+            /*
+            *  Everytime the state is changed to AVAILABLE, only one time is done a check if the waste level is over the WASTE_THRESHOLD.
+            *  If it does, then is saved the actual time and after a predetermined FULL_TOLERANCE_TIME the check is repeated.
+            *  After the second check if the bin appear to be full, then send the FInal State Machine directly to FULL state.
+            */
             if( digitalRead(OPEN_BTN) == HIGH ) { /* When open button is pressed then open bin door */
+                /*
+                *  Every "clock" time the open button state is checked, if the button is pressed then in WasteTask::executeState()
+                *  the bin door will be open.
+                */
                 motor.write(OPEN_ANGLE);
                 userCanSpillTimer.reset();
                 fsm->state = ACCEPTING_WASTE;
@@ -75,6 +84,12 @@ void WasteTask::changeState(){
 
             break;
         case ACCEPTING_WASTE:
+            /*
+            *  When the Final State Machine is in ACCEPTING_WASTE state, it checks every time the close button state.
+            *  If the close button state goes to HIGH (pressed) OR the spill time limit is reached then close the bin and
+            *  send Final State Machine to WASTE_RECEIVED state.
+            *  The "countdown" for spill time limit is started inside AVAILABLE state, before the Final State Machine change the state.
+            */
             if( digitalRead(CLOSE_BTN) == HIGH  || userCanSpillTimer.isOver() ) {
                 motor.write(CLOSED_ANGLE);
                 showAcceptedWasteTimer.reset();
@@ -85,18 +100,36 @@ void WasteTask::changeState(){
             }
             break;
         case WASTE_RECEIVED:
+            /*
+            *  When the Final State Machine is in WASTE_RECEIVED state, it checks every time the bin is closed.
+            *  Then it checks if the waste has reached the WASTE_THRESHOLD and then decide the next state.
+            */
             if(showAcceptedWasteTimer.isOver()){
                 fsm->state = AVAILABLE;
             }
 
             break;
         case EMPTYING:
+            /*
+            *  When the Final State Machine is in EMPTYING state, it starts the release procedure
+            *  opening the bin door in RELEASE position.
+            *  When the bin door is in RELEASE position then wait a RELEASE_TIME and then close the bin door.
+            *  When the bin door is closed then the Final State Machine checks if now the waste level is under
+            *  the WASTE_THRESHOLD.
+            *  If not send Final State Machine to FULL state else send Final State Machine to AVAILABLE state. !!MAYBE NOT!!
+            */
             if(emptyingSimulationTimer.isOver()){
                 motor.write(CLOSED_ANGLE);
                 fsm->state = AVAILABLE;
             }
             break;
         case FULL:
+            /*
+            *  Everytime the state is changed to FULL, only one time is done a check if the waste level is already under the WASTE_THRESHOLD.
+            *  If it does, then is saved the actual time and after a predetermined FULL_TOLERANCE_TIME the check is repeated.
+            *  After the second check if the bin appear to not be full, then send the FInal State Machine directly to AVAILABLE state.
+            *  Else wait for Dashboard signal to send the Final State Machine to EMPTYING state.
+            */
             if(receivedEmptyEvent){
                 motor.write(EMPTYING_ANGLE);
                 fsm->state = EMPTYING;
@@ -104,8 +137,14 @@ void WasteTask::changeState(){
             }
             break;
         case OVERHEATING:
+            /*
+            *  When the Final State Machine is in OVERHEATING state then wait for TemperatureTask to handle the problem,
+            *  WasteTask in this phase only close the bin door if its not closed.
+            */
+            break;
         case SLEEPING:
-            //Do nothing because it's handled in other tasks
+            /* When the Final State Machine is in SLEEPING state WasteTask in this phase only close the bin door if its not closed. */
+            /* On wake up signal SleepTask send the Final State Machine to AVAILABLE */
             break;
     }
 }
